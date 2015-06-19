1abe23e06a1c8d1c412b39b5090b3d6b6ee7e60a
wanghao <odaywanghao@gmail.com>
2015-06-20 00:18:35 +0800

    原来git只能git当前目录下的内容,现在将刚才的全部git了

---
+++ Application/Runtime/Cache/Home/3ef053418a73db4ea9f9fc1fa1038c9d.php
@@ -0,0 +1,5 @@
+<?php if (!defined('THINK_PATH')) exit();?><form action="/yoyoyo/think/index.php/Home/Notes/upload" enctype="multipart/form-data" method="post" >
+<input type="text" name="name" />
+<input type="file" name="photo" />
+<input type="submit" value="提交" >
+</form>
